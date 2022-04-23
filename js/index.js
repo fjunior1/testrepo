3bac2c9b69351e24f863e4a2037d5bf4f2176630
FRanklyn Diaz <franklyn_diaz@hotmail.com>
2022-04-23 19:47:28 -0400

    New repo setup

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$('#btn1a').click(function (event) {
+    // queryAPICityInfo(event.target.innerText);
+    window.alert("clicked");
+    $("btn1").css("background-color", "red");
+})
+
+
+function myFunction(){
+    window.alert("clicked");
+    
+}
