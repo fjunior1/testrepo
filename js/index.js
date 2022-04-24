dc50e4bcea8236a5bce07403304e33cd06cf1c2a
Franklyn Diaz <fdiaz32@gatech.edu>
2022-04-24 12:10:06 -0400

    Fixing issue with button click event handler

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 
-$('#btn1a').click(function (event) {
+document.getElementById('btn1a').onclick =function (event) {
     // queryAPICityInfo(event.target.innerText);
     window.alert("clicked");
-    $("btn1").css("background-color", "red");
-})
-
-
-function myFunction(){
-    // window.alert("clicked");
-    $("btn1").css("background-color", "red");
-    var elem = Document.getElementById('btn1');
-    elem.style.color = "yellow";
+    document.getElementById("btn1a").style.backgroundColor = "red";
 }
+
+// The next commented function is used to handle click
+// event in button when you add
+//  onclick="myFunction()"
+// to the html button element
+
+// function myFunction(){
+//     // window.alert("clicked");
+//     $("btn1a").css("background-color", "red");
+//     var elem = document.getElementById('btn1a')
+//     elem.style.color = "yellow";
+// }
